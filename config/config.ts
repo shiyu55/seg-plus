6a31d2897d3b2d58cd5b582a1a857b8d5b497a9d
ke <baiheinet@gmail.com>
2020-12-21 14:10:43 +0800

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -33,6 +33,9 @@ export default defineConfig({
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
     'primary-color': defaultSettings.primaryColor,
+    'layout-header-background': '#104560', // 自定义 dark siderMenu 需要修改Antd的theme layout-header-background, menu-dark-submenu-bg, menu-dark-item-active-bg
+    'menu-dark-submenu-bg': '#0B3043',
+    'menu-dark-item-active-bg': '#2a6590',
   },
   title: false,
   ignoreMomentLocale: true,
