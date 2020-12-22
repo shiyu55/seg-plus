ab7033086a393989d0e6c88c8d53103fbf80864d
ke <baiheinet@gmail.com>
2020-12-22 15:05:27 +0800

    components add Charts always use

---
+++ src/components/Charts/index.tsx
@@ -0,0 +1,17 @@
+import ChartCard from './ChartCard';
+import Field from './Field';
+import Map from './Map';
+import MiniArea from './MiniArea';
+import MiniBar from './MiniBar';
+import MiniProgress from './MiniProgress';
+
+const Charts = {
+  MiniBar,
+  MiniArea,
+  MiniProgress,
+  ChartCard,
+  Field,
+  Map,
+};
+
+export { Charts as default, MiniBar, MiniArea, MiniProgress, ChartCard, Field, Map };
